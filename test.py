89c0aa3867e14abbdd307bce747a7851c7ba0b02
MOHAMEDELDAKDOUKY <45735497+MOHAMEDELDAKDOUKY@users.noreply.github.com>
2020-01-21 10:25:35 +0200

    Update test.py

--- test.py
+++ test.py
@@ -49,7 +49,8 @@ def evaluate(model, path, iou_thres, conf_thres, nms_thres, img_size, batch_size
             outputs = non_max_suppression_(outputs, conf_thres=conf_thres, nms_thres=nms_thres)
         sample_metrics += get_batch_statistics(outputs, targets, iou_threshold=iou_thres)
     # Concatenate sample statistics\
-    precision =  recall = AP = f1 = ap_class  = np.array([.0])
+    precision=recall=AP=f1 = np.array([0.])
+    ap_class               = np.array([0])
     if len(sample_metrics):
         true_positives, pred_scores, pred_labels = [np.concatenate(x, 0) for x in list(zip(*sample_metrics))]
         precision, recall, AP, f1, ap_class = ap_per_class(true_positives, pred_scores, pred_labels, labels)
