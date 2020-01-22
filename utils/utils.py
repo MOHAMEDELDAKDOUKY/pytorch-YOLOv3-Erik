c181f7c8c8ef9cca8ffc20f021e34bfb71bf8cc9
MOHAMEDELDAKDOUKY <45735497+MOHAMEDELDAKDOUKY@users.noreply.github.com>
2020-01-22 08:11:09 +0200

    Update utils.py

--- utils/utils.py
+++ utils/utils.py
@@ -244,26 +244,29 @@ def non_max_suppression(prediction, conf_thres=0.5, nms_thres=0.4):
         # Object confidence times class confidence
         score = image_pred[:, 4] * image_pred[:, 5:].max(1)[0]
         # Sort by it
-        image_pred = image_pred[(-score).argsort()]
-        class_confs, class_preds = image_pred[:, 5:].max(1, keepdim=True)
-        detections = torch.cat((image_pred[:, :5], class_confs.float(), class_preds.float()), 1)
+        #image_pred = image_pred[(-score).argsort()]
+        # class_confs, class_preds = image_pred[:, 5:].max(1, keepdim=True)
+        # detections = torch.cat((image_pred[:, :5], class_confs.float(), class_preds.float()), 1)
+
+        output[image_i] = image_pred[torchvision.ops.boxes.batched_nms(image_pred[:, :4], score, iou_thres)]
+
         # Perform non-maximum suppression
-        keep_boxes = []
-        while detections.size(0):
-            large_overlap = bbox_iou(detections[0, :4].unsqueeze(0), detections[:, :4]) > nms_thres
-            if not any(large_overlap):
-                detections = np.delete(detections, 0, 0)
-                continue
-            label_match = detections[0, -1] == detections[:, -1]
-            # Indices of boxes with lower confidence scores, large IOUs and matching labels
-            invalid = large_overlap & label_match
-            weights = detections[invalid, 4:5]
-            # Merge overlapping bboxes by order of confidence
-            detections[0, :4] = (weights * detections[invalid, :4]).sum(0) / weights.sum()
-            keep_boxes += [detections[0]]
-            detections = detections[~invalid]
-        if keep_boxes:
-            output[image_i] = torch.stack(keep_boxes)
+        # keep_boxes = []
+        # while detections.size(0):
+        #     large_overlap = bbox_iou(detections[0, :4].unsqueeze(0), detections[:, :4]) > nms_thres
+        #     # if not any(large_overlap):
+        #     #     detections = np.delete(detections, 0, 0)
+        #     #     continue
+        #     label_match = detections[0, -1] == detections[:, -1]
+        #     # Indices of boxes with lower confidence scores, large IOUs and matching labels
+        #     invalid = large_overlap & label_match
+        #     weights = detections[invalid, 4:5]
+        #     # Merge overlapping bboxes by order of confidence
+        #     detections[0, :4] = (weights * detections[invalid, :4]).sum(0) / weights.sum()
+        #     keep_boxes += [detections[0]]
+        #     detections = detections[~invalid]
+        # if keep_boxes:
+        #     output[image_i] = torch.stack(keep_boxes)
 
     return output
     
